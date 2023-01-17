084248047c089b82179c876cfb0999e5960d4826
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-01-17 14:37:42 +0100

    cleaned up some files

--- script/api.js
+++ script/api.js
@@ -1,9 +1,6 @@
 /* export default { getBalance, getTransactions };
-
-
 const YOUR_API_KEY = '6M66CW4F48TDS1AVR3U2DRDD1PAIGJ8QRK';
 const ADDRESS = '0xccec26e3640a0F70808F78118863d045669e271D';
-
 async function getBalance() {
     const url = `https://api-goerli.etherscan.io/api?module=account&action=balance&address=${ADDRESS}&tag=latest&apikey=${YOUR_API_KEY}`;
     try {
@@ -14,10 +11,7 @@ async function getBalance() {
         console.error(error);
     }
 }
-
 getBalance();
-
-
 async function getTransactions() {
     const url = `https://api-goerli.etherscan.io/api?module=account&action=txlist&address=${ADDRESS}&startblock=0&endblock=99999999&sort=asc&apikey=${YOUR_API_KEY}`;
     try {
@@ -28,5 +22,4 @@ async function getTransactions() {
         console.error(error);
     }
 }
-
 getTransactions(); */
