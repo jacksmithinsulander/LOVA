9869e40f261f6a4d911ee13b109fb3ea0d577020
Jack Smith Insulander <106760392+jacksmithinsulander@users.noreply.github.com>
2023-01-26 13:34:56 +0100

    Update sectionOne.js

--- script/htmlExports/sectionOne.js
+++ script/htmlExports/sectionOne.js
@@ -1,6 +1,6 @@
 export const sectionOneHtml = `
 <h1 class="sectionTitles" id="sectionOneTitle">Setting a new standard for promises.</h1>
 <p class="sectionOneSmallText">We believe in a revolution within the DeFi space, and for that 
-trustworthy tools are needed to engage a trustless world of finance.
-(THIS PAGE IS A UI EXAMPLE AND <b>NOT</b> THE FINAL DAPP
+trustworthy tools are needed to engage a trustless world of finance. <br>
+(THIS PAGE IS A UI EXAMPLE AND <b>NOT</b> THE FINAL DAPP)
 </p>`
