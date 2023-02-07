9134311203f81108335d0c8ba4772cfa8a533fc4
Felipe <felipehipsman@hotmail.com>
2023-02-07 11:58:09 +0100

    Fixed mostly styling

--- script/htmlExports/promiseInterfaces.js
+++ script/htmlExports/promiseInterfaces.js
@@ -1,27 +1,28 @@
 export const createPromiseHtml = `
-<summary class="menuTitles fontTitle" >Create</summary>
+<summary class="menuTitles fontTitle" ><img class="interfaceIcon" src="../imgs/createIcon.png">Create</summary>
 <form id="createSmartPromiseInterface" class="bottomInterface">
 
     <input type="text" id="promiseTitle" name="promiseTitle" class="inputStyling" placeholder="Title for your promise"></input>
     <input type="number" id="promiseCollateral" name="promiseCollateral" class="inputStyling" placeholder="Promise collateral" step="0.000000000000000001"></input>
-    <button id="createPromiseBtn" class="interfaceBtns" type="button"><p>Create Promise</p><img class="interfaceIcon" src="../imgs/createIcon.png"> </button>
-    <p id="successfulPromiseUID"  class="sectionOneSmallText" >
+    <button id="createPromiseBtn" class="interfaceBtns" type="button"><p>Create Promise</p> </button>
+    <p id="successfulPromiseUID"  class="sectionOneSmallText"></p>
 
 </form>`
 
 export const joinPromiseHtml = `
-<summary class="menuTitles fontTitle">Join</summary>
+<summary class="menuTitles fontTitle">
+<img class="interfaceIcon" src="../imgs/joinIcon.png">Join</summary>
 
 <form id="joinSmartPromiseInterface" class="bottomInterface">
 
     <input type="text" id="promiseID" class="inputStyling" placeholder="ID of active Promise"></input>
     <p class="interfaceTxt" id="joinPromiseSearchOutput">Promise Details Will Be Displayed here</p>
-    <button id="joinPromiseBtn" class="interfaceBtns" type="button"><p id="joinP">Search Promise</p><img class="interfaceIcon" src="../imgs/joinIcon.png"></button>
+    <button id="joinPromiseBtn" class="interfaceBtns" type="button"><p id="joinP">Search Promise</p></button>
 
 </form>`
 
 export const endPromiseHtml = `
-<summary class="menuTitles fontTitle">End</summary>
+<summary class="menuTitles fontTitle"><img class="interfaceIcon" src="../imgs/endIcon.png">End</summary>
 
 <form id="endSmartPromiseInterface" class="bottomInterface">
 
@@ -33,17 +34,17 @@ export const endPromiseHtml = `
             <option value="end" id="endPromiseOption">End Promise</option>
         </select>
     </div>
-    <button id="endPromiseBtn" class="interfaceBtns" type="button"><p id="endP">Sign Promise</p><img class="interfaceIcon" src="../imgs/endIcon.png"></button>
+    <button id="endPromiseBtn" class="interfaceBtns" type="button"><p id="endP">Sign Promise</p></button>
 
 </form>`
 
 export const searchPromiseHtml = `
-<summary class="menuTitles fontTitle" >Search</summary>
+<summary class="menuTitles fontTitle" ><img class="interfaceIcon" src="../imgs/searchIcon.png">Search</summary>
 
 <form id="searchSmartPromiseInterface" class="bottomInterface">
 
 	<input type="text" id="promiseId" name="promiseTitle" class="inputStyling" placeholder="Promise ID"></input>
     <p class="interfaceTxt" id="searchOutput">Promise participants and Promise Title Displayed here</p>
-	<button id="searchPromiseBtn" class="interfaceBtns" type="button"><p>Search</p><img class="interfaceIcon" src="../imgs/searchIcon.png"></button>
+	<button id="searchPromiseBtn" class="interfaceBtns" type="button"><p>Search</p></button>
 
 </form>`
--- sol/smartPromise.sol
+++ sol/smartPromise.sol
@@ -3,55 +3,70 @@
 pragma solidity ^0.8.3;
 
 contract smartPromiseContract {
-    
     struct promiseData {
         address initialDepositor;
         uint256 promiseCollateral;
         string promiseTitle;
-        uint promiseIdentifier;
-        uint promiseAcceptDeadline;
+        uint256 promiseIdentifier;
+        uint256 promiseAcceptDeadline;
         address[] promiseParticipators;
     }
 
     promiseData[] public smartPromises;
-    mapping(uint => mapping(address => bool)) signed;
-    event SmartPromiseCreated(uint promiseIdentifier);
+    mapping(uint256 => mapping(address => bool)) signed;
+    event SmartPromiseCreated(uint256 promiseIdentifier);
 
     // mapping(promiseData.promiseIdentifier => bool) signed;
 
-    function createSmartPromise(string memory _promiseTitle) public payable { //returns (uint promiseIdentifier) {
+    function createSmartPromise(string memory _promiseTitle) public payable {
+        //returns (uint promiseIdentifier) {
         promiseData memory newPromise;
         newPromise.initialDepositor = msg.sender;
         newPromise.promiseCollateral = msg.value;
         newPromise.promiseTitle = _promiseTitle;
-        newPromise.promiseIdentifier = uint(keccak256(abi.encodePacked(
-            block.difficulty, block.timestamp, block.coinbase))) % 2**160; 
-            //generates a random number to use as a identifier
+        newPromise.promiseIdentifier =
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        block.coinbase
+                    )
+                )
+            ) %
+            2**160;
+        //generates a random number to use as a identifier
         signed[newPromise.promiseIdentifier][msg.sender] = false;
         newPromise.promiseAcceptDeadline = block.timestamp + 10 minutes;
         smartPromises.push(newPromise);
-        promiseData storage arrPushPromise = smartPromises[smartPromises.length - 1];
+        promiseData storage arrPushPromise = smartPromises[
+            smartPromises.length - 1
+        ];
         arrPushPromise.promiseParticipators.push(msg.sender);
         emit SmartPromiseCreated(newPromise.promiseIdentifier);
     }
 
-
-    function joinPromise(uint _promiseUID) public payable {
+    function joinPromise(uint256 _promiseUID) public payable {
         bool ableToJoin;
-        for (uint i = 0; i < smartPromises.length; i++) {
-            if (smartPromises[i].promiseIdentifier == _promiseUID &&
-            smartPromises[i].promiseAcceptDeadline > block.timestamp &&
-            smartPromises[i].promiseCollateral == msg.value) {
+        for (uint256 i = 0; i < smartPromises.length; i++) {
+            if (
+                smartPromises[i].promiseIdentifier == _promiseUID &&
+                smartPromises[i].promiseAcceptDeadline > block.timestamp &&
+                smartPromises[i].promiseCollateral == msg.value
+            ) {
                 ableToJoin = true;
-                smartPromises[i].promiseParticipators.push(msg.sender); 
+                smartPromises[i].promiseParticipators.push(msg.sender);
                 signed[_promiseUID][msg.sender] = false;
                 break;
             }
         }
-        require(ableToJoin, "Error: Invalid promise UID or deadline has passed or collateral does not match");
+        require(
+            ableToJoin,
+            "Error: Invalid promise UID or deadline has passed or collateral does not match"
+        );
     }
 
-    function endSmartPromise(uint _promiseUID) public payable {
+    function endSmartPromise(uint256 _promiseUID) public payable {
         bool ableToWithdraw = true;
         uint256 promiseIndex;
         for (uint256 i = 0; i < smartPromises.length; i++) {
@@ -60,21 +75,37 @@ contract smartPromiseContract {
                 break;
             }
         }
-        require(promiseIndex < smartPromises.length, "Invalid promise identifier");
-        require(smartPromises[promiseIndex].promiseCollateral > 0, "This promise has no collateral to withdraw");
+        require(
+            promiseIndex < smartPromises.length,
+            "Invalid promise identifier"
+        );
+        require(
+            smartPromises[promiseIndex].promiseCollateral > 0,
+            "This promise has no collateral to withdraw"
+        );
 
-        for (uint j = 0; j < smartPromises[promiseIndex].promiseParticipators.length; j++) {
-            if (!signed[_promiseUID][smartPromises[promiseIndex].promiseParticipators[j]]) {
+        for (
+            uint256 j = 0;
+            j < smartPromises[promiseIndex].promiseParticipators.length;
+            j++
+        ) {
+            if (
+                !signed[_promiseUID][
+                    smartPromises[promiseIndex].promiseParticipators[j]
+                ]
+            ) {
                 ableToWithdraw = false;
                 break;
             }
         }
 
         require(ableToWithdraw, "All participants have not signed the promise");
-        payable(smartPromises[promiseIndex].initialDepositor).transfer(smartPromises[promiseIndex].promiseCollateral);
+        payable(smartPromises[promiseIndex].initialDepositor).transfer(
+            smartPromises[promiseIndex].promiseCollateral
+        );
     }
 
-    function signFullfilledPromise(uint _promiseUID) public {
+    function signFullfilledPromise(uint256 _promiseUID) public {
         uint256 promiseIndex;
         for (uint256 i = 0; i < smartPromises.length; i++) {
             if (smartPromises[i].promiseIdentifier == _promiseUID) {
@@ -82,25 +113,52 @@ contract smartPromiseContract {
                 break;
             }
         }
-        require(promiseIndex < smartPromises.length, "Invalid promise identifier");
+        require(
+            promiseIndex < smartPromises.length,
+            "Invalid promise identifier"
+        );
 
         bool isParticipant = false;
-        for (uint j = 0; j < smartPromises[promiseIndex].promiseParticipators.length; j++) {
-            if (smartPromises[promiseIndex].promiseParticipators[j] == msg.sender) {
+        for (
+            uint256 j = 0;
+            j < smartPromises[promiseIndex].promiseParticipators.length;
+            j++
+        ) {
+            if (
+                smartPromises[promiseIndex].promiseParticipators[j] ==
+                msg.sender
+            ) {
                 isParticipant = true;
                 break;
             }
         }
         require(isParticipant, "You are not a participant of this promise");
 
-        require(!signed[_promiseUID][msg.sender], "You have already signed this promise");
+        require(
+            !signed[_promiseUID][msg.sender],
+            "You have already signed this promise"
+        );
         signed[_promiseUID][msg.sender] = true;
     }
 
-    function showPromiseInfo(uint _promiseUID) public view returns (address[] memory, string memory, uint256) {
-        for (uint i = 0; i < smartPromises.length; i++) {
+    function showPromiseInfo(uint256 _promiseUID)
+        public
+        view
+        returns (
+            address[] memory,
+            string memory,
+            uint256,
+            uint256
+        )
+    {
+        for (uint256 i = 0; i < smartPromises.length; i++) {
             if (smartPromises[i].promiseIdentifier == _promiseUID) {
-                return(smartPromises[i].promiseParticipators, smartPromises[i].promiseTitle, smartPromises[i].promiseCollateral);
+                return (
+                    smartPromises[i].promiseParticipators,
+                    smartPromises[i].promiseTitle,
+                    smartPromises[i].promiseCollateral,
+                    smartPromises[i].promiseAcceptDeadline
+                );
             }
         }
         revert("Promise not found");
